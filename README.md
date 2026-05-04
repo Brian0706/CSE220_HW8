77b57d9fea4e175076d01224104bf3da7fe4abbb
Brian Chau <brianchau0729@gmail.com>
2026-05-03 20:21:15 -0400

    Improved clarity of hw8a and added tests for it to readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # CSE220_HW8
+
+Part A tests:
+
+Test 1 - Smallest Triangle
+Input: 0, 1
+Result: 
+*
+
+Test 2 - Smallest Square
+Input: 1, 1
+Result: 
+*
+
+Test 3 - Smallest Pyramid
+Input: 2, 1
+Result: 
+*
+
+Test 4 - Base Triangle
+Input: 5, 1
+Result: 
+*
+
+Test 5 - Base Square
+Input: 5, 1
+Result: 
+*
+
+Test 6 - Base Pyramid
+Input: 5, 1
+Result: 
+*
+
--- hw8a.asm
+++ hw8a.asm
@@ -1,181 +1,214 @@
 .data
 typeprompt: .asciiz "Triangle(0) or Square(1) or Pyramid (2)? "
 sizeprompt: .asciiz "Required size? "
-newline: .asciiz "\n"
 invalid_shape: .asciiz "Must give a 0,1,2 for the shape"
 invalid_size: .asciiz "The size must be a positive number\n"
-star: .asciiz "*"
-space: .asciiz " "
 
 .text 
 main:
-li $t2, 0            #Used to check if $t0 == 0
-li $t3, 1            #Used to check if $t0 == 1
-li $t4, 2            #Used to check if $t0 == 1
+	li $t2, 0            #Used to check if shape argument given is 0 for a triangle
+	li $t3, 1            #Used to check if shape argument given is 1 for a square
+	li $t4, 2            #Used to check if shape argument given is 2 for a pyramid
+	
+	#Prints out prompt to type in shape type
+	li $v0, 4
+	la $a0, typeprompt   #printf("Triangle(0) or Square(1) or Pyramid (2)? ");
+	syscall
 
-li $v0, 4
-la $a0, typeprompt   #printf("Triangle(0) or Square(1) or Pyramid (2)? ")
-syscall
+	#Read shape selection from user
+	li $v0, 5            
+	syscall
+	move $t0, $v0        #scanf("%d",&type);
+	
+	li $v0, 11
+	la $a0, '\n'      #printf("\n");
+	syscall
+	
+	#Print out prompt to type in size
+	li $v0, 4
+	la $a0, sizeprompt   #printf("Required size? ");
+	syscall
 
-li $v0, 5            
-syscall
-move $t0, $v0        #scanf("%d",&type)
+	#Read size from user 
+	li $v0, 5            
+	syscall
+	move $t1, $v0        #scanf("%d",&size);
 
-li $v0, 4
-la $a0, newline      #printf("\n")
-syscall
-la $a0, sizeprompt   #printf("Required size? ")
-syscall
-
-li $v0, 5            
-syscall
-move $t1, $v0        #scanf("%d",&size)
-
-li $v0, 4
-la $a0, newline      #printf("\n")
-syscall
-
-bgt $t1, $zero, valid_size   #if(size > 0) goto valid_size
-la $a0, invalid_size         #printf("The size must be a positive number\n")
-syscall
-j done  
+	li $v0, 11
+	la $a0, '\n'      #printf("\n");
+	syscall
+	
+	#Check if user gave a valid size
+	bgt $t1, $zero, valid_size   #if(size > 0) goto valid_size
+	la $a0, invalid_size         #printf("The size must be a positive number\n")
+	syscall
+	j done  
 
 valid_size:
-bne $t0, $t2, not_triangle   #if(type != 0) goto not_triangle
-move $a0, $t1                #a0 = size
-move $a1, $zero              #a1 = 0 
-jal triangle                 #triangle(size, 0)                
-j done
+	#Check if user asked for a triangle and print one if so
+	bne $t0, $t2, not_triangle   #if(type != 0) goto not_triangle
+	move $a0, $t1                #a0 = size;
+	move $a1, $zero              #a1 = 0;
+	jal triangle                 #triangle(size, 0);               
+	j done
 
 not_triangle:
-bne $t0, $t3, not_square    #if(type != 1) goto not_square
-move $a0, $t1               #a0 = size
-move $a1, $zero             #a1 = 0 
-jal square                  #square(size, 0)  
-j done
+	#Check if user asked for a square and print one if so
+	bne $t0, $t3, not_square    #if(type != 1) goto not_square
+	move $a0, $t1               #a0 = size
+	move $a1, $zero             #a1 = 0 
+	jal square                  #square(size, 0)  
+	j done
 
 not_square:
-bne $t0, $t4, not_shape     #if(type != 2) goto not_shape
-move $a0, $t1               #a0 = size
-move $a1, $zero             #a1 = 0 
-jal pyramid                 #pyramid(size, 0)
-j done
+	#Check if user asked for a pyramid and print one if so
+	bne $t0, $t4, not_shape     #if(type != 2) goto not_shape
+	move $a0, $t1               #a0 = size
+	move $a1, $zero             #a1 = 0 
+	jal pyramid                 #pyramid(size, 0)
+	j done
 
 not_shape:
-la $a0, invalid_shape
-li $v0, 4           #printf("Must give a 0,1,2 for the shape")
-syscall
+	#Tell user they did not give a valid shape type
+	li $v0, 4           #printf("Must give a 0,1,2 for the shape")
+	la $a0, invalid_shape
+	syscall
 
 done:
-li $v0, 10
-syscall
+	li $v0, 10
+	syscall
 
 
-
-square:          #square(int width, int curHeight)
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-
-beq $a0, $a1, s_end       #if(width == curHeight) goto s_end
-
-li $t1, 0                #counter = 0
-move $t2, $a0            #t2 = width
-li $v0, 4
+#square(int width, int curHeight)
+square: 
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	#Check for base case
+	beq $a0, $a1, s_reset     #if(width == curHeight) goto s_end
+	
+	#Prepare loop variables
+	li $t1, 0                #counter = 0
+	move $t2, $a0            #t2 = width
 
 s_loop: 
-la $a0, star            #a0 = "*"
-syscall                 #printf("*")
-addi $t1, $t1, 1        #counter = counter + 1
-blt $t1, $t2, s_loop      #if(counter < width) goto s_loop
-
-la $a0, newline         #a0 = "\n"
-syscall                 #printf("\n")
-move $a0, $t2           #a0 = width
-addi $a1, $a1, 1        #curHeight = curHeight + 1
-jal square              #square(width, curHeight + 1)
+	#Print a line of the square
+	li $v0, 11
+	la $a0, '*'             #a0 = '*'
+	syscall                 #printf("%c",a0);
+	addi $t1, $t1, 1        #counter = counter + 1
+	blt $t1, $t2, s_loop    #if(counter < width) goto s_loop
 
 s_end:
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
+	#Print a newline and recursively call to print next line
+	li $v0, 11
+	la $a0, '\n'           #a0 = '\n'
+	syscall                #printf("%c",a0);
+	move $a0, $t2          #a0 = width
+	addi $a1, $a1, 1       #curHeight = curHeight + 1
+	jal square             #square(width, curHeight + 1)
+
+s_reset:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
 
 
-
-triangle:       #triangle(int width, int curHeight)
-addi $sp, $sp, -4
-sw $ra, 0($sp)
-
-beq $a0, $a1, t_end       #if(width == curHeight) goto t_end
-
-li $t1, 0                #counter = 0
-move $t2, $a0            #t2 = width
-
-li $v0, 4
+#triangle(int width, int curHeight)
+triangle: 
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	#Check for base case
+	beq $a0, $a1, t_reset       #if(width == curHeight) goto t_end
+	
+	#Prepare loop variables
+	li $t1, 0                 #counter = 0
+	move $t2, $a0             #t2 = width
 
 t_loop: 
-la $a0, star            #a0 = "*"
-syscall                 #printf("*")
-addi $t1, $t1, 1        #counter = counter + 1
-ble $t1, $a1, t_loop    #if(counter <= curHeight) goto t_loop
-
-la $a0, newline         #a0 = "\n"
-syscall                 #printf("\n")
-move $a0, $t2           #a0 = width
-addi $a1, $a1, 1        #curHeight = curHeight + 1
-jal triangle            #triangle(width, curHeight + 1)
+	#Print a line of the triangle
+	li $v0, 11
+	la $a0, '*'               #a0 = '*'
+	syscall                   #printf("%c",a0);
+	addi $t1, $t1, 1          #counter = counter + 1
+	ble $t1, $a1, t_loop      #if(counter <= curHeight) goto t_loop
 
 t_end:
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
+	#Print a newline and recursively call to print next line
+	li $v0, 11
+	la $a0, '\n'             #a0 = '\n'
+	syscall                 #printf("%c",a0);
+	move $a0, $t2           #a0 = width
+	addi $a1, $a1, 1        #curHeight = curHeight + 1
+	jal triangle            #triangle(width, curHeight + 1)
 
-pyramid:       #pyramid(int width, int curHeight)
-addi $sp, $sp, -4
-sw $ra, 0($sp)
+t_reset:
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
 
-beq $a0, $a1, p_end       #if(width == curHeight) goto t_end
+#pyramid(int width, int curHeight)
+pyramid:      
+	addi $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	#Check for base case
+	beq $a0, $a1, p_end       #if(width == curHeight) goto t_end
 
-li $t1, 0                #counter = 0
-move $t2, $a0            #t2 = width
+	#Prepare loop variables
+	li $t1, 0                #counter = 0
+	move $t2, $a0            #t2 = width
 
-sub $t3, $a0, $a1        #bounds = width - curHeight
-addi $t3, $t3, -1        #bounds = bounds - 1
-move $a0, $t3
-li $v0, 4
+	#Determine how many spaces before and after stars
+	sub $t3, $a0, $a1        #bounds = width - curHeight
+	addi $t3, $t3, -1        #bounds = bounds - 1
+	move $a0, $t3
 
 l_bound:
-bge $t1, $t3, l_end      #if(counter < bounds) goto l_end
-la $a0, space            #a0 = " "
-syscall                  #printf(" ")
-addi $t1, $t1, 1         #counter = counter + 1
-j l_bound                #goto l_bound
+	#Print left boundary
+	li $v0, 11
+	bge $t1, $t3, l_end      #if(counter < bounds) goto l_end
+	la $a0, ' '              #a0 = ' ';
+	syscall                  #printf("%c", a0);
+	addi $t1, $t1, 1         #counter = counter + 1
+	j l_bound                #goto l_bound
 l_end:
-
-li $t1, 0                #counter = 0
+	#Reset counter
+	li $t1, 0                #counter = 0
+	
 middle:
-la $a0, star             #a0 = "*"
-syscall                  #printf("*")
-la $a0, space            #a0 = " "
-syscall                  #printf(" ")
-addi $t1, $t1, 1        #counter = counter + 1
-ble $t1, $a1, middle    #if(counter <= curHeight) goto middle
+	#Print out the middle section of stars and spaces alternating
+	li $v0, 11
+	la $a0, '*'              #a0 = "*"
+	syscall                  #printf("%c",a0)
+	la $a0, ' '              #a0 = " "
+	syscall                  #printf("%c",a0)
+	addi $t1, $t1, 1         #counter = counter + 1
+	ble $t1, $a1, middle     #if(counter <= curHeight) goto middle
+	
+middle_end:
+	li $t1, 0                #counter = 0
 
-li $t1, 0                #counter = 0
 r_bound:
-bge $t1, $t3, r_end    #if(counter < bounds) goto r_end
-la $a0, space            #a0 = " "
-syscall                  #printf(" ")
-addi $t1, $t1, 1         #counter = counter + 1
-j r_bound                #goto r_bound
+	#Print right boundary
+	li $v0, 11
+	bge $t1, $t3, r_end     #if(counter < bounds) goto r_end
+	la $a0, ' '             #a0 = " "
+	syscall                 #printf('%c', a0)
+	addi $t1, $t1, 1        #counter = counter + 1
+	j r_bound               #goto r_bound
+	
 r_end:
-
-la $a0, newline         #a0 = "\n"
-syscall                 #printf("\n")
-move $a0, $t2           #a0 = width
-addi $a1, $a1, 1        #curHeight = curHeight + 1
-jal pyramid             #pyramid(width, curHeight + 1)
+	#Print a newline and recursively call to print next line
+	li $v0, 11
+	la $a0, '\n'            #a0 = '\n'
+	syscall                 #printf('%c', a0)
+	move $a0, $t2           #a0 = width
+	addi $a1, $a1, 1        #curHeight = curHeight + 1
+	jal pyramid             #pyramid(width, curHeight + 1)
 
 p_end:
-lw $ra, 0($sp)
-addi $sp, $sp, 4
-jr $ra
+	lw $ra, 0($sp)
+	addi $sp, $sp, 4
+	jr $ra
