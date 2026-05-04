099d7c7039f75867e7478d1c647836910be2162d
Brian Chau <brianchau0729@gmail.com>
2026-05-03 20:36:41 -0400

    Improved clarity of hw8b and added tests for it to readme

--- README.md
+++ README.md
@@ -32,3 +32,26 @@ Input: 5, 1
 Result: 
 *
 
+Part B tests:
+Test 1 - Same values
+Input: 1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,0,0
+Result: 1 1|2 2|3 3|4 4|5 5|6 6|7 7|8 8|9 9|0 0|
+
+Test 2 - Base case
+Input: 1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0
+Result: 2 1|4 3|6 5|8 7|0 9|2 1|4 3|6 5|8 7|0 9|
+
+Test 3 - Negative numbers
+Input: 1,-2,-3,4,-5,6,-7,-8,9,0,1,-2,-3,4,5,6,-7,-8,9,0
+Result: -2 1|4 -3|6 -5|-8 -7|0 9|-2 1|4 -3|6 5|-8 -7|0 9|
+
+Test 4 - Different n
+- Set n to 5
+Input: 1,2,3,4,5,6,7,8,9,0
+Result: 2 1|4 3|6 5|8 7|0 9|
+
+Test 4 - Smallest case
+- Set n to 1
+Input: 1,2
+Result: 2 1|
+
--- hw8b.asm
+++ hw8b.asm
@@ -12,50 +12,53 @@ space: .asciiz " "
 
 .text
 main:
-la $t0, A        #pointerA = A
-la $t1, B        #pointerB = B
-lw $t2, num      #size = num
-li $t3, 1        #counter = 1
+	#Load in variables
+	la $t0, A        #Create a pointer to A
+	la $t1, B        #Create a pointer to B
+	lw $t2, num      #size = num
+	#Because matrix is zero-indexed but count starts at 1
+	#counter is initalized to 1
+	li $t3, 1        #counter = 1
 
+#Gets intial value from users
 init_loop:
-bgt $t3, $t2, init_end  #if(counter > num) goto init_end
-
-li $v0, 4
-la $a0, startA          #printf("A[")
-syscall
-li $v0, 1
-move $a0, $t3
-syscall                 #printf("%d", counter)
-li $v0, 4
-la $a0, ending
-syscall                 #printf("]=")
-li $v0, 5              
-syscall                 
-sw $v0, 0($t0)          #scanf("%d", pointerA)
-li $v0, 4
-la $a0, newline
-syscall                 #printf("\n")
-
-li $v0, 4
-la $a0, startB          #printf("B[")
-syscall
-li $v0, 1
-move $a0, $t3
-syscall                 #printf("%d", counter)
-li $v0, 4
-la $a0, ending
-syscall                 #printf("]=")
-li $v0, 5              
-syscall                 
-sw $v0, 0($t1)          #scanf("%d", pointerB)
-li $v0, 4
-la $a0, newline
-syscall                 #printf("\n")
-
-addi $t3, $t3, 1        #counter = counter + 1
-addi $t0, $t0, 4        #pointerA = pointerA + 1
-addi $t1, $t1, 4        #pointerB = pointerB + 1
-j init_loop             #goto init_loop
+	bgt $t3, $t2, init_end  #if(counter > num) goto init_end
+	
+	#This section prints out A[counter]=
+	li $v0, 4
+	la $a0, startA          #printf("A[")
+	syscall
+	li $v0, 1
+	move $a0, $t3
+	syscall                 #printf("%d", counter)
+	li $v0, 4
+	la $a0, ending
+	syscall                 #printf("]=")
+	#Gets value for A[counter-1] from user
+	li $v0, 5              
+	syscall                 
+	sw $v0, 0($t0)          #scanf("%d", pointerA)
+	
+	#This section prints out A[counter]=
+	li $v0, 4
+	la $a0, startB          #printf("B[")
+	syscall
+	li $v0, 1
+	move $a0, $t3
+	syscall                 #printf("%d", counter)
+	li $v0, 4
+	la $a0, ending
+	syscall                 #printf("]=")
+	#Gets value for B[counter-1] from user
+	li $v0, 5              
+	syscall                 
+	sw $v0, 0($t1)          #scanf("%d", pointerB)
+	
+	
+	addi $t3, $t3, 1        #Increment counter
+	addi $t0, $t0, 4        #Move pointer to next element in A
+	addi $t1, $t1, 4        #Move pointer to next element in B
+	j init_loop             #goto init_loop
 init_end: 
 
 jal swap
@@ -65,44 +68,53 @@ jal print_loop
 li $v0, 10
 syscall
 
+#Swaps the values of matrix A and matrix B
 swap:
-la $t0, A               #pointerA = A
-la $t1, B               #pointerB = B
-li $t3, 1               #counter = 1
-swap_loop:
-bgt $t3, $t2, swap_end  #if(counter > num) goto swap_end
-lw $t4, 0($t0)          #temp = *pointerA
-lw $t5, 0($t1)          #temp2 = *pointerB
-sw $t4, 0($t1)          #*pointerA = temp2
-sw $t5, 0($t0)          #*pointerB = temp
-addi $t3, $t3, 1        #counter = counter + 1
-addi $t0, $t0, 4        #pointerA = pointerA + 1
-addi $t1, $t1, 4        #pointerB = pointerB + 1
-j swap_loop
-swap_end:
-jr $ra
+	la $t0, A               #pointer to matrix A
+	la $t1, B               #pointer to matrix B
+	li $t3, 1               #counter = 1
 
+swap_loop:
+	bgt $t3, $t2, swap_end  #if(counter > num) goto swap_end
+	#Store A[counter-1] and B[counter-1] in registers
+	lw $t4, 0($t0)          #temp = *pointerA
+	lw $t5, 0($t1)          #temp2 = *pointerB
+	#Swap the values of the arrays
+	sw $t4, 0($t1)          #*pointerA = temp2
+	sw $t5, 0($t0)          #*pointerB = temp
+	
+	addi $t3, $t3, 1        #Increment counter
+	addi $t0, $t0, 4        #Move pointer to next element in A
+	addi $t1, $t1, 4        #Move pointer to next element in B
+	j swap_loop             #goto swap_loop
+swap_end:
+	jr $ra
+
+#Prints out loops A and B as specified in the assignment
 print_loop:
-la $t0, A               #pointerA = A
-la $t1, B               #pointerB = B
-li $t3, 1               #counter = 1
+	la $t0, A               #Create a pointer to A
+	la $t1, B               #Create a pointer to B
+	li $t3, 1               #counter = 1
+	
+#Loops through arrays and prints them out
 out_loop:
-bgt $t3, $t2, out_end  #if(counter > num) goto out_end
-li $v0, 1
-lw $a0, 0($t0)
-syscall                #printf("%d", *pointerA)
-li $v0, 4
-la $a0, space
-syscall                #printf(" ")
-li $v0, 1
-lw $a0, 0($t1)
-syscall                #printf("%d", *pointerB)
-li $v0, 4
-la $a0, border
-syscall                #printf("|")
-addi $t3, $t3, 1        #counter = counter + 1
-addi $t0, $t0, 4        #pointerA = pointerA + 1
-addi $t1, $t1, 4        #pointerB = pointerB + 1
-j out_loop             #goto out_loop
+	#Print out value of A[counter - 1] and B[counter-1], then |
+	bgt $t3, $t2, out_end  #if(counter > num) goto out_end
+	li $v0, 1
+	lw $a0, 0($t0)
+	syscall                #printf("%d", *pointerA)
+	li $v0, 4
+	la $a0, space
+	syscall                #printf(" ")
+	li $v0, 1
+	lw $a0, 0($t1)
+	syscall                #printf("%d", *pointerB)
+	li $v0, 4
+	la $a0, border
+	syscall                #printf("|")
+	addi $t3, $t3, 1        #counter = counter + 1
+	addi $t0, $t0, 4        #pointerA = pointerA + 1
+	addi $t1, $t1, 4        #pointerB = pointerB + 1
+	j out_loop             #goto out_loop
 out_end:
-jr $ra
+	jr $ra
